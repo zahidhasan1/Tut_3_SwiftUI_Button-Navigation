a594e1c62b0bd8bca45649ebaf00e28fe31c13ff
Zahid Hasan <megamind.d.evilll@gmail.com>
2022-07-27 13:02:44 +0600

    Initial Commit

---
+++ Tut_3_SwiftUI_Button&Navigation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tut_3_SwiftUI_Button&Navigation
+//
+//  Created by ZEUS on 27/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tut_3_SwiftUI_Button&Navigation/Tut_3_SwiftUI_Button_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tut_3_SwiftUI_Button_NavigationApp.swift
+//  Tut_3_SwiftUI_Button&Navigation
+//
+//  Created by ZEUS on 27/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Tut_3_SwiftUI_Button_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
